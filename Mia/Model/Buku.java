ce37d12a7520710b92895d7511a982fb2f5b632c
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-06-15 10:20:36 +0700

    Create Buku.java

---
+++ Mia/Model/Buku.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia.Model;
+
+/**
+ *
+ * @author user
+ */
+public class Buku {
+  private String kodebuku;
+  private String judul;
+  private String pengarang;
+  private String penerbit;
+  
+  public Buku (){
+      
+  }
+
+    public String getKodebuku() {
+        return kodebuku;
+    }
+
+    public void setKodebuku(String kodebuku) {
+        this.kodebuku = kodebuku;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    public void setPengarang(String pengarang) {
+        this.pengarang = pengarang;
+    }
+
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+
+  
+
+  
+   
+
+  
+
+  
+  
+  
+          
+    
+    
+}
