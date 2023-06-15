2ef318f6ee4b6bc7e6d2ab072b40ec39c044a955
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-06-15 10:19:41 +0700

    Create PengembalianDao.java

---
+++ Mia/Dao/PengembalianDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia.Dao;
+
+import Mia.Model.Peminjaman;
+import Mia.Model.Pengembalian;
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ *
+ * @author user
+ */
+public interface PengembalianDao {
+    
+    public void insert(Connection con, Pengembalian pengembalian) throws Exception;
+    public void update(Connection con, Pengembalian pengembalian) throws Exception;
+    public void delete(Connection con, Pengembalian pengembalian) throws Exception;
+    public Pengembalian getPengembalian(Connection con, 
+    String kodeanggota, String kodebuku, String tglpinjam) throws Exception;
+    public List<Pengembalian> getAllPengembalian(Connection con) throws Exception;
+    
+}
