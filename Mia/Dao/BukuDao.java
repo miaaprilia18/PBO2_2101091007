a22cae83229ec505da46fe118f0ad9b1b0c1b67e
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-06-15 10:18:16 +0700

    Create BukuDao.java

---
+++ Mia/Dao/BukuDao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia.Dao;
+
+import Mia.Model.Buku;
+import java.util.List;
+import java.sql.Connection;
+
+/**
+ *
+ * @author user
+ */
+public interface BukuDao {
+    
+   public void insert(Connection con, Buku buku) throws Exception;
+    public void update(Connection con, Buku buku) throws Exception;
+    public void delete(Connection con, Buku buku) throws Exception;
+    public Buku getBuku(Connection con, String kode) throws Exception;
+    public List<Buku> getAllBuku(Connection con) throws Exception;
+    
+}
